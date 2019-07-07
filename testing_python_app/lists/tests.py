340da7cd097ac267257afa5ea373ef4a6db47768
Runping Huang <runping@shanshu.ai>
2019-07-07 22:28:29 +0800

    first view

--- testing_python_app/lists/tests.py
+++ testing_python_app/lists/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-# Create your tests here.
+# Create your tests here. Write this first!!!
 class SmokeTest(TestCase):
     def test_fail(self):
         self.assertEqual(1+1,3)
+
+class HomePageTest(TestCase):
+    def test_root_url_resolvable(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
--- testing_python_app/testing_python_app/urls.py
+++ testing_python_app/testing_python_app/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from lists.views import home_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    url(r"^$", home_page, name="home")
 ]
