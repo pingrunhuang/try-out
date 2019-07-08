0252ce787a7dd57463aa45b8aff46a55cbe11160
Runping Huang <runping@shanshu.ai>
2019-07-08 08:54:40 +0800

    first TDD process finished

--- testing_python_app/lists/tests.py
+++ testing_python_app/lists/tests.py
@@ -1,13 +1,21 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here. Write this first!!!
 class SmokeTest(TestCase):
     def test_fail(self):
-        self.assertEqual(1+1,3)
+        self.assertEqual(1+2,3)
 
 class HomePageTest(TestCase):
     def test_root_url_resolvable(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
+    
+    def test_home_page(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b"<html>"))
+        self.assertContains(response, b"To-Do")
+        self.assertTrue(response.content.endswith(b"</html>"))
